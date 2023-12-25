64ca8b4d535957bdd048f26a96635d678dcb5d8d
darlingson <dmakuwila@gmail.com>
2023-12-25 16:47:22 +0200

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -66,4 +67,57 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Material design
+    implementation("com.google.android.material:material:1.11.0")
+
+    //Archetectural Components
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    //kotlin extension and coroutines support for Room
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    //Coroutines lifecycle scopes
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    ksp("com.github.bumptech.glide:compiler:4.15.1")
+
+    //Google Maps Location Services
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    //Dagger Core
+    implementation("com.google.dagger:dagger:2.48")
+    ksp("com.google.dagger:dagger-compiler:2.46.1")
+
+    //Dagger Android
+    api("com.google.dagger:dagger-android:2.46.1")
+    api("com.google.dagger:dagger-android-support:2.46.1")
+    ksp("com.google.dagger:dagger-android-processor:2.46.1")
+
+    //Easy permissions
+    implementation("pub.devrel:easypermissions:3.0.0")
+
+    //Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    //MPAndroidChart
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    implementation("android.arch.lifecycle:extensions:1.1.1")
 }
